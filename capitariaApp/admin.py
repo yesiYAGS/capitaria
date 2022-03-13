a4602f776a261dfe94a3a7ab0de7eb36c545de6c
yesiYAGS <yesibelgonza@gmail.com>
2022-03-12 23:00:35 -0300

    creacion de superuser para usar django admin

--- capitariaApp/admin.py
+++ capitariaApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Profesor)
+admin.site.register(Curso)
+admin.site.register(Prueba)
+admin.site.register(Estudiante)
 
 # Register your models here.
